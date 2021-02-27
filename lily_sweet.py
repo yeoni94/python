8cf5f1224ff9973009b2c2397863fe17983b96b7
kgy <gayeon906@gmail.com>
2021-02-26 21:50:17 -0800

    파이썬 마지막 주차

--- lily_sweet.py
+++ lily_sweet.py
@@ -11,7 +11,7 @@ def Keyboard():
     return jsonify(dataSend)
 
 
-@app.route('/message', methods=['GET'])
+@app.route('/message', methods=['POST'])
 def Message():
     content = request.get_json()
     content = content['userRequest']
@@ -93,6 +93,82 @@ def Message():
                 ]
             }
         }
+    elif content == u"편의점":
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type": "basicCard",
+                            "items": [
+                                {
+                                    "title": "",
+                                    "description": "편의점별 대표 과자를 알려드릴께요. \n어떤 편의점이 궁금하세요?(GS,CU,세븐일레븐)"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    elif content == u"GS":
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type": "basicCard",
+                            "items": [
+                                {
+                                    "title": "",
+                                    "description": "GS편의점 대표 과자는 다음과 같습니다. \n* GS25 초코렛타 \n* GS25 버터갈릭팝콘 \n* GS25 초코써니볼 \n* GS25 쌀로 만든 고소한 인절미"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    elif content == u"CU":
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type": "basicCard",
+                            "items": [
+                                {
+                                    "title": "",
+                                    "description": "CU편의점 대표 과자는 다음과 같습니다. \n* CU 초코바나나콘 \n* CU 콘소메맛 팝콘 \n* CU 까르보나라스낵 \n* CU 꼬불이 라면스낵"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    elif content == u"세븐일레븐":
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type": "basicCard",
+                            "items": [
+                                {
+                                    "title": "",
+                                    "description": "세븐일레븐 대표 과자는 다음과 같습니다. \n* 세븐일레븐 트위스트 \n* 세븐일레븐 씨앗호떡뻥"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
     else:
         dataSend = {
             "version": "2.0",
@@ -100,8 +176,7 @@ def Message():
                 "outputs": [
                     {
                         "simpleText": {
-                            "text": "저는 과자 추천 봇입니다. 오늘의 기분에 따른 과자를 추천해줍니다.\n"
-                                    +"당신의 기분을 알려주세요.(신남, 우울, 짜증, 화남)"
+                            "text": "저는 과자 추천 봇입니다. 오늘의 기분에 따른 과자를 추천해줍니다.\n당신의 기분을 알려주세요.(신남, 우울, 짜증, 화남)"
                         }
                     }
                 ]
